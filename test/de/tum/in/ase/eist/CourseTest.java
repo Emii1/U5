4d91cc403beba224224451e3fb960a491f92cdc9
Emi <emi.mano@tum.de>
2022-07-02 10:19:42 +0200

    ab

--- test/de/tum/in/ase/eist/CourseTest.java
+++ test/de/tum/in/ase/eist/CourseTest.java
@@ -2,28 +2,34 @@ package de.tum.in.ase.eist;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CourseTest {
 
-	// TODO 1: Test getCourseTitle()
-    @Test
-    public void getCourseTitle(){
-        Course course = new Course();
 
-        Assertions.assertTrue(course.getTitle().contains(course.getTitle()));
+
+    @Test
+    
+    public void getCourseTitle(){
+        //given
+        String name = "Eist";
+        Course course = new Course( name);
+
+        Assertions.assertEquals(name,course.getTitle());
+
  }
 
 	// TODO 2: Test getNumberOfAttendees()
     @Test
+
     public void testNoAttendees(){
        Course course = new Course();
        Assertions.assertEquals(0,course.getNumberOfAttendees());
     }
-    @Test
-    public void testThreeAttendees(){
-        Course course = new Course();
-        Assertions.assertEquals(3,course.getNumberOfAttendees());
-    }
+    //@Test
+
+   // public void testThreeAttendees(){
+     //   Course course = new Course();
+
+    //    Assertions.assertEquals(3,course.getNumberOfAttendees());
+   // }
 
 }
